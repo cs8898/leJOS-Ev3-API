a9423a02308e0809afb1076055729271848768c0
Christian Schmied <cs8898@gmx.de>
2015-04-17 16:05:39 +0200

    Fixed Motor Control, doc will be added soon

--- src/de/cs8898/lejos/ev3/api/motors/MotorControl.java
+++ src/de/cs8898/lejos/ev3/api/motors/MotorControl.java
@@ -10,6 +10,25 @@ public class MotorControl{
 	}
 	
 	public void rotate(int speed,int direction, int angle, boolean immediateReturn){
+		this.setSpeed(speed, direction);
+		this.motorLeft.startSynchronization();
+		this.motorRight.startSynchronization();
+		this.motorLeft.rotate(angle, immediateReturn);
+		this.motorRight.rotate(angle, immediateReturn);
+		this.motorLeft.endSynchronization();
+		this.motorRight.endSynchronization();
+	}
+
+	public double getMaxSpeed() {
+		return (this.motorLeft.getMaxSpeed()<this.motorRight.getMaxSpeed()?this.motorLeft.getMaxSpeed():this.motorRight.getMaxSpeed());
+	}
+
+	public void setSpeed(int speed) {
+		this.motorLeft.setSpeed(speed);
+		this.motorRight.setSpeed(speed);
+	}
+	
+	public void setSpeed(int speed, int direction){
 		int[] motorSpeed = {speed,speed};
 		
 		int dir = -1;
@@ -18,17 +37,42 @@ public class MotorControl{
 		else if (direction > 0)
 			dir = 1;
 		
-		double uDir = Math.pow(Math.pow(direction, 2),0.5);
+		double uDir = Math.abs(direction);
 		
 		if (dir > -1)
-			motorSpeed[dir] = (int)(50-uDir)/50*speed;
+			motorSpeed[dir] = (int)((50-uDir)/50*speed);
 		
-		this.motorLeft.startSynchronization();
-		this.motorRight.startSynchronization();
 		this.motorLeft.setSpeed(motorSpeed[0]);
 		this.motorRight.setSpeed(motorSpeed[1]);
-		this.motorLeft.rotate(angle, immediateReturn);
-		this.motorRight.rotate(angle, immediateReturn);
+	}
+	
+	public void stop(){
+		this.stop(false);
+	}
+	
+	public void stop(boolean immediateReturn){
+		this.motorLeft.startSynchronization();
+		this.motorRight.startSynchronization();
+		this.motorLeft.stop(immediateReturn);
+		this.motorRight.stop(immediateReturn);
+		this.motorLeft.endSynchronization();
+		this.motorRight.endSynchronization();
+	}
+	
+	public void forward(){
+		this.motorLeft.startSynchronization();
+		this.motorRight.startSynchronization();
+		this.motorLeft.forward();
+		this.motorRight.forward();
+		this.motorLeft.endSynchronization();
+		this.motorRight.endSynchronization();
+	}
+	
+	public void backward(){
+		this.motorLeft.startSynchronization();
+		this.motorRight.startSynchronization();
+		this.motorLeft.backward();
+		this.motorRight.backward();
 		this.motorLeft.endSynchronization();
 		this.motorRight.endSynchronization();
 	}
